2ea7ed93c83c902c16d780373f8d0f9bed48fa07
Sasha Gerrand <sgerrand@users.noreply.github.com>
2025-01-27 10:09:27 +0000

    Configure project to publish to Hex

--- mix.exs
+++ mix.exs
@@ -7,22 +7,35 @@ defmodule BambooHR.MixProject do
       version: "0.0.0-dev",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Hex
+      description: "Elixir client for the Bamboo HR API",
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Sasha Gerrand"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/sgerrand/ex_bamboo_hr",
+        "Sponsor" => "https://github.com/sponsors/sgerrand"
+      },
+      files: ~w(lib LICENSE mix.exs README.md)
     ]
   end
 end
