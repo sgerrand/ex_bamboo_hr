382b72683b368d3abae8a903dc4a3ade8c1a35d7
Sasha Gerrand <sgerrand@users.noreply.github.com>
2025-01-28 11:45:34 +0000

    Remove scaffolded functions

--- lib/bamboo_hr.ex
+++ lib/bamboo_hr.ex
@@ -2,17 +2,4 @@ defmodule BambooHR do
   @moduledoc """
   Documentation for `BambooHR`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> BambooHR.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
