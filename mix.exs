cf7d024a5898060e40937d8afee7a06ccedc97ee
Sasha Gerrand <sgerrand@users.noreply.github.com>
2025-03-04 17:09:49 +0000

    Define project version

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule BambooHR.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :bamboo_hr,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
